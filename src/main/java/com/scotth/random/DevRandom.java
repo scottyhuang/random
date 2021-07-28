f0b1bf2e69f899ab28bb1838f27617d844d1b624
Scott Huang <scott.y2k.huang@gmail.com>
2021-07-28 17:53:57 -0400

    Add Unit Tests

--- src/main/java/com/scotth/random/DevRandom.java
+++ src/main/java/com/scotth/random/DevRandom.java
@@ -2,74 +2,111 @@ package com.scotth.random;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
-import java.util.Properties;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.commons.crypto.random.CryptoRandom;
 import org.apache.commons.crypto.random.CryptoRandomFactory;
 
 /**
- * DevRandom clones /dev/random
- * Using Apache Commons Crypto 
- *
+ * DevRandom simulates linux /dev/random
+ * Depends on Apache Commons Crypto package 
+ * https://commons.apache.org/proper/commons-crypto/
+ * Default implementation is the procesor's hardware Digital Random Number Generator 
+ * (DRNG). 
+ * Backup implementation is Java SecureRandom cryptographically 
+ * strong RNG.
+ * 
  */
-class DevRandom {
-    static final Properties properties;
-    static String fileName = "dev-random";
-    static final int offSet = 4;
+public class DevRandom {
+    private static final String DEFAULT_FILE_PATH = "devrandom";
+    private static final int BUFFER_SIZE = 4;
+  
+    private static CryptoRandom random = null;
 
     static {
-        properties = new Properties();
-        properties.put(CryptoRandomFactory.CLASSES_KEY,
-            CryptoRandomFactory.RandomProvider.OPENSSL.getClassName());
+        initializeRandom();
     }
 
-    static CryptoRandom random = initializeRandom();
-
-    private static CryptoRandom initializeRandom() {
+    public static void initializeRandom() {
         try {
-            return CryptoRandomFactory.getCryptoRandom(properties);
+            DevRandom.random = CryptoRandomFactory.getCryptoRandom();
         } catch (GeneralSecurityException e) {
-            return null;
+            System.err.println(e.getMessage());
         }
     }
-   
+
+    private static void writeNextRandomBytes(
+        CryptoRandom random, OutputStream out, byte[] b, int off, int len) 
+            throws IOException{
+        random.nextBytes(b);
+        out.write(b);
+    }
+
     /**
-    * @param   path    - the path to the file
-    * @param   size    - an object to iterate over the char sequences
-    * truncate and overwrite an existing file, or create the file if
-    * it doesn't initially exist
+    * Writes random bytes to default output file continuously
+    * @throws GeneralSecurityException if CryptoRandom is not created
+    * @throws IOException if IO errors
     */
-    public static void writeRandomBytes(String path, int size) throws IOException{
+    public static void writeRandomBytes() 
+        throws IOException, GeneralSecurityException {
+            writeRandomBytes(DevRandom.DEFAULT_FILE_PATH, -1);
+    }
+
+    /**
+    * Writes Random Bytes to output file continuously
+    * @param   path    - path of the output file
+    * @throws GeneralSecurityException if CryptoRandom is not created
+    * @throws IOException if IO errors
+    */
+    public static void writeRandomBytes(String path) 
+        throws IOException, GeneralSecurityException {
+            writeRandomBytes(path, -1);
+    }
+
+    /**
+    * @param   path    - path of the output file
+    * @param   size    - size of the output file in bytes
+    * @throws GeneralSecurityException if CryptoRandom is not created
+    * @throws IOException if IO errors
+    */
+    public static void writeRandomBytes(String path, int size) throws 
+        IOException, GeneralSecurityException {
         if (random == null) {
-            random = initializeRandom();
+            throw new GeneralSecurityException("CryptoRandom Object Not Found");
         }
 
+        if (path == null || path.length() < 1) {
+            path = DevRandom.DEFAULT_FILE_PATH;
+        }
+        
         Path file = Paths.get(path);
         OutputStream out = null;
-        int step = offSet;
-        byte[] b = new byte[offSet];
+        
+        byte[] b = new byte[DevRandom.BUFFER_SIZE];
 
         try {
             out = Files.newOutputStream(file);
-            int i;
             
             if (size < 0) {
                 while(true) {
-                    random.nextBytes(b);
-                    out.write(b);  
+                    writeNextRandomBytes(random, out, b, 0, DevRandom.BUFFER_SIZE);
                 }
             } 
 
-            for (i=0; i < size - offSet; i+=step) {    
-                random.nextBytes(b);
-                out.write(b);
+            for (int i=0; i < size; i += DevRandom.BUFFER_SIZE) { 
+                int length = size - i < DevRandom.BUFFER_SIZE ? size -i : DevRandom.BUFFER_SIZE;
+                writeNextRandomBytes(random, out, b, 0, length);
             }
-            if (i < size) {
-                random.nextBytes(b);
-                out.write(b, 0, size - i);
-            }
+           
         } finally {
             if (out != null) {
                 out.close();
@@ -77,7 +114,43 @@ class DevRandom {
         }
     }
 
-    public static void main(final String []args) throws IOException {
-       writeRandomBytes("./dev/random", -1);
+    /**
+    * @param -f,--filepath   output file path
+    * @param -s,--size       output file size in bytes
+    */
+    public static void main(final String []args) 
+        throws IOException, GeneralSecurityException{
+        Options options = new Options();
+        options.addOption("f", "filepath", false, "output file path")
+               .addOption("s", "size", false, "output file size in bytes");
+                  
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("DevRandom", options);
+
+        CommandLineParser parser = new DefaultParser();
+        String path = null;
+        int size = 0;
+
+        try {
+            CommandLine cmd = parser.parse( options, args);
+            if (cmd.hasOption("f")) {
+                path = cmd.getOptionValue("f");
+            }
+            if (cmd.hasOption("s")) {
+                String sizeStr = cmd.getOptionValue("s");
+                try {
+                    if (sizeStr != null) {
+                        size = Integer.parseInt(sizeStr);
+                    }
+                } catch (NumberFormatException e) {
+                    size = 0;
+                }
+            }
+        } catch (ParseException e) {
+            System.err.println("Error encountered Parsing Arguments. " + e.getMessage());
+            return;
+        }
+        
+        writeRandomBytes(path, size);
     }
 }
--- src/test/java/com/scotth/random/DevRandomTest.java
+++ src/test/java/com/scotth/random/DevRandomTest.java
@@ -1,19 +1,53 @@
 package com.scotth.random;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.io.File;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
- * Unit test for simple App.
+ * Unit test for DevRandom
  */
 public class DevRandomTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( 2 > 1 );
+    private static final String DEFAULT_FILE_PATH = "./dev/random-test";
+
+    private static long getFileLength(String filePath) throws IOException {
+        File file = new File(filePath);
+        return file.length();
     }
+
+    /**
+     * Test DevRandom.writeRandomBytes()
+     * using parameterized output file size
+     * and default output file path
+     *
+     */
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 4, 128, 512 })
+    public void testWriteRandomBytesWithSizeArgument(int argument)
+    {
+        try{
+            DevRandom.writeRandomBytes(DevRandomTest.DEFAULT_FILE_PATH, argument);
+            long fileSize = getFileLength(DevRandomTest.DEFAULT_FILE_PATH);
+            assertEquals(fileSize, argument);
+        } catch (Exception e) {
+            assertEquals("exception", e.getMessage());
+        }
+    }
+
+    @AfterAll
+    public static void performOneTimeTeardown() {
+        File file = new File(DevRandomTest.DEFAULT_FILE_PATH);
+        try {
+            Files.deleteIfExists(file.toPath());
+        } catch (IOException e) {
+            System.err.println("Unable to cleanup test file. " + e.getMessage());
+        }
+    }    
 }
